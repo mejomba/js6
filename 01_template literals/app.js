d91d2bf266c26231c645d3e5bc2d4b11040c31c9
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-05-26 18:44:29 +0430

    template literals

---
+++ 01_template literals/app.js
@@ -0,0 +1,19 @@
+const name = 'mojtaba';
+const lastName = 'aminzadeh';
+
+const fullName = name + ' ' + lastName;
+console.log(fullName);
+
+const newName = `${name} ${lastName}`;
+console.log(newName);
+
+// const example = `${5+2} ${name}`;
+// const example = "hello \n world";
+const example = `hello
+
+
+world`;
+console.log(example);
+
+myDiv = document.querySelector('#mydiv');
+myDiv.innerText = example;
