0592c9f4ab1c0b2cd7d660219cd28d2955d3986e
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-06-21 21:56:11 +0430

    aa

---
+++ 08_arrow function/app.js
@@ -0,0 +1,11 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+function name(a, b) {
+    return a + b;
+}
+a = []
+
+btn = document.querySelector('#btn');
+btn.addEventLisenar('click', function () {
+    console.log('click btn');
+})
